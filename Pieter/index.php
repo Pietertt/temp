55c7856400618556ef35a1bae4002249a428bbae
Pieter Boersma <pieterboersma20@gmail.com>
2020-03-16 17:01:00 +0100

    The login UI is done

--- Pieter/index.php
+++ Pieter/index.php
@@ -4,39 +4,40 @@
             <link rel="stylesheet" href="../style.css">
       </header>
       <body>
-
-            <div class="four wide container">
-                  <div class="row">
-                        <div class="four wide column">
-                              <h3 class="selected">Inloggen</h3>
-                        </div>
-                        <div class="four wide column">
-                              <h3 class="deselected">Registreren</h3>
+            <div class="five wide white rounded container">
+                  <div class="ten wide container">
+                        <div class="row">
+                              <div class="four wide column">
+                                    <h2 class="selected">Inloggen</h2>
+                              </div>
+                              <div class="four wide column">
+                                    <h2 class="deselected">Registreren</h2>
+                              </div>
                         </div>
                   </div>
-            </div>
-            <div class="four wide container">
-                  <form action="post">
-                        <div class="row">
-                              <div class="twelve wide column">
-                                    <label for="email">E-mailadres</label>
-                                    <input id="email" name="email" type="email" required>
+                  <div class="ten wide container">
+                        <form action="post">
+                              <div class="row">
+                                    <div class="twelve wide column">
+                                          <label for="email">E-mailadres</label>
+                                          <input id="email" name="email" type="email" required>
+                                    </div>
                               </div>
-                        </div>
 
-                        <div class="row">
-                              <div class="twelve wide column">
-                                    <label for="password">Wachtwoord</label>
-                                    <input id="password" name="password" type="password" required>
+                              <div class="row">
+                                    <div class="twelve wide column">
+                                          <label for="password">Wachtwoord</label>
+                                          <input id="password" name="password" type="password" required>
+                                    </div>
                               </div>
-                        </div>
 
-                        <div class="row">
-                              <div class="twelve wide column">
-                                    <button class="blue button">Versturen</button>
+                              <div class="row">
+                                    <div class="twelve wide column">
+                                          <button class="twelve wide blue button">Versturen</button>
+                                    </div>
                               </div>
-                        </div>
-                  </form>
+                        </form>
+                  </div>
             </div>
       </body>
 </html>
