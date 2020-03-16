9205653bd666d318b72c7e5f5cdcfef563c3d00c
Pieter Boersma <pieterboersma20@gmail.com>
2020-03-16 15:41:07 +0100

    Form template done

--- Pieter/index.php
+++ Pieter/index.php
@@ -1,9 +1,51 @@
 <!DOCTYPE html>
 <html>
       <header>
-
+            <link rel="stylesheet" href="../style.css">
       </header>
       <body>
-            
+            <div class="row">
+                  <div class="container">
+                        <div class="row">
+                              <div class="col-3">
+                                    <h3>Inloggen</h3>
+                              </div>
+                              <div class="col-3">
+                                    <h3>Registreren</h3>
+                              </div>
+                        </div>
+                  </div>
+                  <div class="container">
+                        <form action="post">
+                              <div class="row">
+                                    <div class="col-4">
+                                          <label for="email">E-mailadres</label>
+                                    </div>
+                              </div>
+                              <div class="row">
+                                    <div class="col-4">
+                                          <input id="email" name="email" type="email" required>
+                                    </div>
+                              </div>
+
+                              <div class="row">
+                                    <div class="col-4">
+                                          <label for="password">Wachtwoord</label>
+                                    </div>
+                              </div>
+                              <div class="row">
+                                    <div class="col-4">
+                                          <input id="password" name="password" type="password" required>
+                                    </div>
+                              </div>
+
+                              <div class="row">
+                                    <div class="col-4">
+                                          <button class="blue button">Versturen</button>
+                                    </div>
+                              </div>
+                        </form>
+                  </div>
+            </div>
       </body>
 </html>
