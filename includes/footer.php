e3f3efd900dcb1350753e8d9814d8c34e118809d
Jan Minne <janminne@gmail.com>
2020-03-23 12:25:12 +0100

    Julian is een lul

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,13 @@
+<div class="row">
+        <div class="twelve wide container">
+    <footer class="twelve wide column">
+        <div class="">
+           <h1>Testing 123</h1>
+        </div>
+    </footer>
+    </div>
+</div>
+
 <script type="text/javascript" src="../js/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
 <script type="text/javascript" src="../js/javascript.js"></script>
 </body>
