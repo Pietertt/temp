602660f3f76bfcda03efb5cb77c816df46152fb3
Jan Minne <janminne@gmail.com>
2020-03-26 11:21:21 +0100

    Error melding beginsel

--- jmh/contact.php
+++ jmh/contact.php
@@ -12,36 +12,44 @@ require("../contactvalidations.php");
 
 <div class="five wide white rounded container">
 <div class="ten wide container">
-    <form method="post">
         <div class="row">
             <div class="twelve wide column">
                 <label for="name">Naam</label>
-                <input id="name" name="name" type="text" placeholder="Uw naam" required>
+                <input id="name" name="name" type="text" placeholder="Uw naam">
             </div>
         </div>
 
         <div class="row">
             <div class="twelve wide column">
                 <label for="email">E-mailadres</label>
-                <input id="email" name="email" type="email" placeholder="Uw email adres" required>
+                <input id="email" name="email" type="text" placeholder="Uw email adres">
             </div>
         </div>
 
         <div class="row">
             <div class="twelve wide column">
                 <label for="message">Bericht</label>
-                <textarea id="message" name="message" type="text" placeholder="Typ hier uw bericht.." rows="5" cols="30" required></textarea>
+                <textarea id="message" name="message" type="text" placeholder="Typ hier uw bericht.." rows="5" cols="30"></textarea>
             </div>
+        </div>
+
+        <div id="errorfield" class="invisible error">
+        <div class="row">
+            <div class="twelve wide column">
+                <p id="errormessage">JE SHIT IS STUK</p>
+            </div>
+        </div>
         </div>
 
         <div class="row">
             <div class="twelve wide column">
-                <button class="twelve wide blue button">Versturen</button>
+                <button id="submit" class="twelve wide blue button">Versturen</button>
             </div>
         </div>
-    </form>
 </div>
 </div>
 
+
+<script src="error.js"></script>
 
 <?php require('../includes/footer.php');?>
---
+++ jmh/error.js
@@ -0,0 +1,15 @@
+document.getElementById("submit").addEventListener("click", createerror);
+
+function createerror() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("errorfield").classList.remove('invisible');
+            console.log("Fuckyou");
+        }
+    };
+    xhttp.open("GET", "", true);
+    xhttp.send();
+
+};
+
