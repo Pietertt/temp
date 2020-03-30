ec54a1e50949f569e7a134a1e0ff4362cf9b4a4a
Pieter Boersma <pieterboersma20@gmail.com>
2020-03-30 14:55:52 +0200

    Bezig met de user modellen

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,5 +1,6 @@
 <?php
       include("../includes/navbar.php");
+      include("../database.php");
 
       $cookie = new Cookie("token");
       if($cookie->does_cookie_exist()){
@@ -20,7 +21,43 @@
                   }
             }
       }
+
+      $database = new database();
+      $database->connect("127.0.0.1", "root", "", "ritsemabanck");
+
+      $cookie = new Cookie("token");
+      $token = Token::decode($cookie->get_value());
+
+      $stmt = $database->get_connection()->prepare("SELECT * FROM `user` WHERE email = ?");
+      $stmt->bind_param("s", $e);
+      $e = $token->username;
+      $stmt->execute();
+      $rows = $stmt->get_result()->fetch_assoc();
+
+      $database->disconnect();
 ?>
+
+<div class="ten wide container">
+      <div class="row">
+            <div class="twelve wide column">
+                  <h1>Dashboard</h1>
+            </div>
+      </div>
+      <div class="row">
+            <div class="three wide column">
+                  <h3>Persoonlijke gegevens</h3>
+                  <p><b>Naam</b></p>
+                  <p><b>Geslacht</b></p>
+                  <p><b>Geboortedatum</b></p>
+                  <p><b>Woonadres</b></p>
+            </div>
+            <div class="three wide column">
+                  <h3>Contactgegevens</h3>
+                  <p><b>Telefoonnummer</b></p>
+                  <p><b>E-mailadres</b></p>
+            </div>
+      </div>
+</div>
 
 <?php
       include("../includes/footer.php");
---
+++ user.php
@@ -0,0 +1,12 @@
+<?php
+      class User {
+            $id;
+            $firstname;
+            $lastname;
+            $gender;
+            $birth_date;
+            $place;
+            $phone_number;
+            $email;            
+      }
+?>
