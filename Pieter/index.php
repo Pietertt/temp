31e429602e773d88d77f46fcbc4e10264f426ca2
Pieter Boersma <pieterboersma20@gmail.com>
2020-03-30 14:19:34 +0200

    Navbar is gefixt

--- Pieter/index.php
+++ Pieter/index.php
@@ -1,5 +1,4 @@
 <?php 
-      require('../includes/header.php');
       require('../includes/navbar.php');
 ?>
 
--- dashboard/index.php
+++ dashboard/index.php
@@ -1,5 +1,4 @@
 <?php
-      include("../includes/header.php");
       include("../includes/navbar.php");
 
       $cookie = new Cookie("token");
