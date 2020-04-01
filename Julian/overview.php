807827f851671982c5b17f5692f2217c699b3b21
julianhulzebosch1997 <43810186+julianhulzebosch1997@users.noreply.github.com>
2020-04-01 14:41:31 +0200

    page

--- Julian/overview.php
+++ Julian/overview.php
@@ -2,24 +2,89 @@
 
 ?>
 
-<section
-<form action=".." method="">
-    <h1><B>Persoonlijke gegevens</B></h1>
+<div class="row toppad">
+    <div class="five wide centered container">
+        <div class="ten wide container">
+            <h3>Persoonlijke gegevens</h3>
+        </div>
+    </div>
+</div>
 
-    <span>Naam:</span> <?php echo $first_name['first_name'];?>
-    <span>Naam:</span> <?php echo $firstname['firstname'];?>
+<div class="five wide white rounded container">
+    <div class="ten wide container">
+        <form method="post" action=
+            <div class="row">
+                <div class="twelve wide column">
+                    <label for="name">Naam</label>
+                    <?php if(isset($_GET['name'])){
+                        $name = $_GET['name'];
+                        echo '<input id="name" name="name" type="text" placeholder="Uw naam" value="'.$name.'">';
+                    }?>
+                </div>
 
-</form>
+            <div class="twelve wide column">
+                <label for="name">Geslacht</label>
+                <?php if(isset($_GET['gender'])){
+                    $gender = $_GET['gender'];
+                    echo '<input id="gender" name="gender" type="text" placeholder="Uw geslacht" value="'.$gender.'">';
+                }?>
 
-<form action=".." method="">
-    <h1><B>Contact gegevens</B></h1>
+            </div>
 
-    <span>Telefoon:</span> <?php echo $Telephone['Telephone'];?>
-    <span>Email:</span> <?php echo $Email['Email'];?>
+            <div class="twelve wide column">
+                <label for="name">Geboortedatum</label>
+                <?php if(isset($_GET['birth_date'])){
+                    $birth_date = $_GET['birth_date'];
+                    echo '<input id="birth_date" name="birth_date" type="text" placeholder="Uw geboortedatum" value="'.$birth_date.'">';
+                }?>
+            </div>
 
-</form>
+            <div class="twelve wide column">
+                <label for="name">Woonplaats</label>
+                <?php if(isset($_GET['residence'])){
+                    $residence = $_GET['residence'];
+                    echo '<input id="residence" name="residence" type="text" placeholder="Uw woonplaats" value="'.$residence.'">';
+                }?>
+                        <img id="residence" data-popup='{"title" : "Verander je woonplaats", "inputs" : [{"description" : "Woonplaats", "name" : "residence", "id" : "residence"}], "button" : { "label" : "Verstuur", "id" : "button_change_residence" }}' src="../img/pen.png">
+            </div>
+        </form>
+    </div>
+</div>
+
+<div class="row toppad">
+    <div class="five wide centered container">
+        <div class="ten wide container">
+            <h3>Contactgegevens</h3>
+        </div>
+    </div>
+</div>
+<div class="five wide white rounded container">
+    <div class="ten wide container">
+        <form method="post" action=
+        <div class="row">
+
+    <div class="twelve wide column">
+    <label for="name">Telefoonnummer</label>
+    <?php if(isset($_GET['tnumber'])){
+        $tnumber = $_GET['tnumber'];
+        echo '<input id="tnumber" name="tnumber" type="text" placeholder="Uw telefoonnummer" value="'.$tnumber.'">';
+    }?>
+
+                    <img id="phone_number" data-popup='{"title" : "Verander je telefoonnummer", "inputs" : [{"description" : "Telefoonnummer", "name" : "phone_number", "id" : "input_phone_number"}], "button" : { "label" : "Verstuur", "id" : "button_change_phone_number" }}' src="../img/pen.png">
+
+    <div class="twelve wide column">
+        <label for="name">Email</label>
+        <?php if(isset($_GET['email'])){
+            $email = $_GET['email'];
+            echo '<input id="email" name="email" type="text" placeholder="Uw email" value="'.$email.'">';
+        }?>
+                    <img id="email" src="../img/pen.png" data-popup=\'{"title" : "Verander je e-mailadres", "inputs" : [{"description" : "E-mailadres", "name" : "email_address", "id" : "input_email_adress"}], "button" : {"label" : "Verstuur", "id" : "button_change_email"}}\'>
+</div>
+    </div>
+        </form>
+    </div>
+</div>
 
 
-</section>
 <?php require('../includes/footer.php');?>
 
