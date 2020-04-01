dda92b6dc29030f8e10558df3cc84856970a84a2
Pieter Boersma <pieterboersma20@gmail.com>
2020-04-01 19:44:59 +0200

    Nieuwe test done

--- tests/cookie/CookieTest.php
+++ tests/cookie/CookieTest.php
@@ -10,7 +10,10 @@
 
             protected function setUp() : void {
                   $this->cookie = new Cookie("test");
+            }
 
+            // checks whether the expiration date is valid
+            public function test_check_expiration_date_invalid(){
                   // creates a test Tok object
                   $tok = new Tok();
                   $tok->username = "Pieter";
@@ -21,11 +24,26 @@
                   // mocks the decode method from the token class so it will not interfere
                   $token = $this->createMock(Token::class);
                   $token->method('decode')->willReturn($tok);
+
+                  // the timestamp is way back, so the method will return false
+                  $this->assertFalse($this->cookie->check_expiration_date("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IlBpZXRlciIsInBhc3N3b3JkIjoidGVzdCIsInRpbWVzdGFtcCI6MTU4NTc1MjIxMCwidmVyaWZpZWQiOjB9.F1s3rwjJUbXeLvBYizCrsW5aWtUH5kh7vGO4bZ2XENQ"), false);
             }
 
-            // checks whether the expiration date is valid
-            public function test_check_expiration_date_invalid(){
-                  $this->assertTrue($this->cookie->check_expiration_date("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IlBpZXRlciIsInBhc3N3b3JkIjoidGVzdCIsInRpbWVzdGFtcCI6MTU4NTc1MjIxMCwidmVyaWZpZWQiOjB9.F1s3rwjJUbXeLvBYizCrsW5aWtUH5kh7vGO4bZ2XENQ"), false);
+            public function test_verify_true(){
+                  $tok = new Tok();
+                  $tok->username = "Pieter";
+                  $tok->password = "test";
+                  $tok->timestamp = 1585752210;
+                  $tok->verified = 0;
+
+                  // mocks the decode method from the token class so it will not interfere
+                  $token = $this->createMock(Token::class);
+                  $token->method('decode')->willReturn($tok);
+
+                  $cookie = $this->createMock(Cookie::class);
+                  $cookie->method('get_value')->willReturn("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IlBpZXRlciIsInBhc3N3b3JkIjoidGVzdCIsInRpbWVzdGFtcCI6MTU4NTc1MjIxMCwidmVyaWZpZWQiOjF9.5MBtBsZSqUcFTiMLFLamU2H040Pr5oRfFECTlvB6Vog");
+
+                  $this->assertTrue($this->cookie->verify($cookie->get_value()), true);
             }
 
             protected function tearDown() : void {
