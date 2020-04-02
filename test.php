24ae1554599dc417149480db7d240b4473401296
Pieter Boersma <pieterboersma20@gmail.com>
2020-04-02 09:51:24 +0200

    Bezig met het versleutelen van de payload

--- test.php
+++ test.php
@@ -12,7 +12,32 @@
       //       $pdf->Cell(0, 10, 'Belangrijke informatie', 0, 1);
       // $pdf->Output();
 
-      $cookie = new Cookie("token");
-      print_r($cookie->get_value());
+      $token = Token::encode("Pietertje", time(), 0);
+      print_r(Token::decode($token));
 
+?>
+
+<?php
+// $key = "12345";
+// //$key should have been previously generated in a cryptographically safe way, like openssl_random_pseudo_bytes
+// $plaintext = "message to be encrypted";
+// $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+// $iv = openssl_random_pseudo_bytes($ivlen);
+// $ciphertext_raw = openssl_encrypt($plaintext, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
+// $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+// $ciphertext = base64_encode( $iv.$hmac.$ciphertext_raw );
+
+// print($ciphertext);
+
+// $c = base64_decode($ciphertext);
+// $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+// $iv = substr($c, 0, $ivlen);
+// $hmac = substr($c, $ivlen, $sha2len=32);
+// $ciphertext_raw = substr($c, $ivlen+$sha2len);
+// $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
+// $calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+// if (hash_equals($hmac, $calcmac))//PHP 5.6+ timing attack safe comparison
+// {
+//     echo $original_plaintext."\n";
+// }
 ?>
--- token.php
+++ token.php
@@ -15,31 +15,54 @@
                   $this->encode();
             }
 
-            public static function encode($username, $password, $time_stamp, $verified): string {
+            public static function encode($username, $time_stamp, $verified): string {
+                  $key = "123";
+
                   $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
-                  $payload = json_encode(['username' => $username, 'password' => $password, 'timestamp' => $time_stamp, 'verified' => $verified]);
+                  $payload = json_encode(['username' => $username, 'timestamp' => $time_stamp, 'verified' => $verified]);
                   
                   $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
                   $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+
+                  $ivlen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
+                  $iv = openssl_random_pseudo_bytes($ivlen);
+                  $ciphertext_raw = openssl_encrypt($base64UrlPayload, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
+                  $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
+                  $ciphertext = base64_encode($iv . $hmac . $ciphertext_raw);
                   
                   $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, 'fkdj54jgj!$&dfj', true);
                   $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
-                  return $base64UrlHeader . "." . password_hash($base64UrlPayload, PASSWORD_BCRYPT) . "." . $base64UrlSignature;
+                  return $base64UrlHeader . "." . $ciphertext . "." . $base64UrlSignature;
             }
 
-            public static function decode($token) : Tok {
+            public static function decode($token) {
+                  $key = "123";
                   $encoded = explode('.', $token)[1];
-                  $replaced = str_replace(['-', '_', ''], ['+', '/', '='], $encoded);
-                  $base64 = base64_decode($replaced);
-                  $json = json_decode($base64);
+                  
+                  $c = base64_decode($encoded);
+                  $ivlen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
+                  $iv = substr($c, 0, $ivlen);
+                  $hmac = substr($c, $ivlen, $sha2len = 32);
+                  $ciphertext_raw = substr($c, $ivlen + $sha2len);
+                  $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
+                  $calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
 
-                  $token = new Tok();
-                  $token->username = $json->username;
-                  $token->password = $json->password;
-                  $token->timestamp = $json->timestamp;
-                  $token->verified = $json->verified;
+                  if (hash_equals($hmac, $calcmac)){
+                        $replaced = str_replace(['-', '_', ''], ['+', '/', '='], $original_plaintext);
+                        $base64 = base64_decode($replaced);
+                        $json = json_decode($base64);
+                        
+                        $json = json_decode($base64);
 
-                  return $token;
+                        $token = new Tok();
+                        $token->username = $json->username;
+                        $token->timestamp = $json->timestamp;
+                        $token->verified = $json->verified;
+
+                        return $token;
+                  } else {
+                        return new Tok();
+                  }
             }
 
             public static function verify($token) : Tok {
