ab2975168895015aa14c90e70fb55cc255eaf7d3
julianhulzebosch1997 <43810186+julianhulzebosch1997@users.noreply.github.com>
2020-04-02 15:32:29 +0200

    update

--- Julian/overview.php
+++ Julian/overview.php
@@ -7,6 +7,8 @@
                   }
             }
       }
+
+
 ?>
 
 <div class="row toppad">
@@ -22,36 +24,32 @@
         <form method="post" action=
             <div class="row">
                 <div class="twelve wide column">
-                    <label for="name">Naam</label>
-                    <?php if(isset($_GET['name'])){
-                        $name = $_GET['name'];
-                        echo '<input id="name" name="name" type="text" placeholder="Uw naam" value="'.$name.'">';
-                    }?>
+                    <label for="name">Naam :</label>
+                    <?php
+                    print_r($_SESSION['user']->firstname);
+                    ?>
                 </div>
 
             <div class="twelve wide column">
-                <label for="name">Geslacht</label>
-                <?php if(isset($_GET['gender'])){
-                    $gender = $_GET['gender'];
-                    echo '<input id="gender" name="gender" type="text" placeholder="Uw geslacht" value="'.$gender.'">';
-                }?>
+                <label for="name">Geslacht :</label>
+                <?php
+                print_r($_SESSION['user']->gender);
+                ?>
 
             </div>
 
             <div class="twelve wide column">
-                <label for="name">Geboortedatum</label>
-                <?php if(isset($_GET['birth_date'])){
-                    $birth_date = $_GET['birth_date'];
-                    echo '<input id="birth_date" name="birth_date" type="text" placeholder="Uw geboortedatum" value="'.$birth_date.'">';
-                }?>
+                <label for="name">Geboortedatum :</label>
+                <?php
+                print_r($_SESSION['user']->birth_date);
+                ?>
             </div>
 
             <div class="twelve wide column">
-                <label for="name">Woonplaats</label>
-                <?php if(isset($_GET['residence'])){
-                    $residence = $_GET['residence'];
-                    echo '<input id="residence" name="residence" type="text" placeholder="Uw woonplaats" value="'.$residence.'">';
-                }?>
+                <label for="name">Woonplaats :</label>
+                <?php
+                print_r($_SESSION['user']->residence);
+                ?>
                         <img id="residence" data-popup='{"title" : "Verander je woonplaats", "inputs" : [{"description" : "Woonplaats", "name" : "residence", "id" : "residence"}], "button" : { "label" : "Verstuur", "id" : "button_change_residence" }}' src="../img/pen.png">
             </div>
         </form>
@@ -65,26 +63,25 @@
         </div>
     </div>
 </div>
+
 <div class="five wide white rounded container">
     <div class="ten wide container">
         <form method="post" action=
         <div class="row">
 
     <div class="twelve wide column">
-    <label for="name">Telefoonnummer</label>
-    <?php if(isset($_GET['tnumber'])){
-        $tnumber = $_GET['tnumber'];
-        echo '<input id="tnumber" name="tnumber" type="text" placeholder="Uw telefoonnummer" value="'.$tnumber.'">';
-    }?>
+    <label for="name">Telefoonnummer :</label>
+        <?php
+        print_r($_SESSION['user']->phone_number);
+        ?>
 
                     <img id="phone_number" data-popup='{"title" : "Verander je telefoonnummer", "inputs" : [{"description" : "Telefoonnummer", "name" : "phone_number", "id" : "input_phone_number"}], "button" : { "label" : "Verstuur", "id" : "button_change_phone_number" }}' src="../img/pen.png">
 
     <div class="twelve wide column">
-        <label for="name">Email</label>
-        <?php if(isset($_GET['email'])){
-            $email = $_GET['email'];
-            echo '<input id="email" name="email" type="text" placeholder="Uw email" value="'.$email.'">';
-        }?>
+        <label for="name">Email :</label>
+        <?php
+        print_r($_SESSION['user']->email);
+        ?>
                     <img id="email" src="../img/pen.png" data-popup='{"title" : "Verander je e-mailadres", "inputs" : [{"description" : "E-mailadres", "name" : "email_address", "id" : "input_email_adress"}], "button" : {"label" : "Verstuur", "id" : "button_change_email"}}\'>
 </div>
     </div>
@@ -96,12 +93,15 @@
     <div class="five wide centered container">
         <div class="ten wide container">
             <h3>Uw hypotheekaanvragen</h3>
+
+            <div class="twelve wide column">
+                <label for="hypotheeken"> Hypotheken : </label>
+                <?php
+                print_r($_SESSION['hypotheeken']->hypotheek);
+                ?>
+            </div>
         </div>
     </div>
-</div>
-
-<div class="twelve wide column">
-
 </div>
 
 
