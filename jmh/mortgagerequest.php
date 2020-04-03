00cd24578b144512fe197fb39f8e165856fd0668
Jan Minne <janminne@gmail.com>
2020-04-03 19:48:54 +0200

    Julian z'n fouten opgelost, prutser :) -xXx-

--- jmh/mortgagerequest.php
+++ jmh/mortgagerequest.php
@@ -15,6 +15,7 @@ require("../jmh/mortgagevalidations.php");
 <div class="five wide white rounded container">
     <div class="ten wide container">
         <form method="post" action="<?php echo htmlspecialchars("http://localhost/temp/jmh/mortgagevalidations.php");?>">
+
             <div class="row">
                 <div class="twelve wide column">
                     <label for="name">Geboortedatum</label>
@@ -30,33 +31,96 @@ require("../jmh/mortgagevalidations.php");
 
             <div class="row">
                 <div class="twelve wide column">
-                    <label for="email">E-mailadres</label>
-                    <?php if(isset($_GET['email'])){
-                        $email = $_GET['email'];
-                        echo '<input id="email" name="email" type="text" placeholder="Uw email adres" value="'.$email.'">';
+                    <label for="gross_anual_income">Bruto jaarinkomen</label>
+                    <?php if(isset($_GET['gross_anual_income'])){
+                        $gross_anual_income = $_GET['gross_anual_income'];
+                        echo '<input id="gross_anual_income" name="gross_anual_income" type="text" placeholder="€" value="'.$gross_anual_income.'">';
                     }
                     else {
-                        echo '<input id="email" name="email" type="text" placeholder="Uw email adres">';
+                        echo '<input id="gross_anual_income" name="gross_anual_income" type="text" placeholder="€">';
                     }?>
                 </div>
             </div>
 
-<!--            <div class="row">-->
-<!--                <div class="twelve wide column">-->
-<!--                    <label for="message">Bericht</label>-->
-<!--                    --><?php //if(isset($_GET['message'])){
-//                        $message = $_GET['message'];
-//                        echo '<textarea id="message" name="message" type="text" placeholder="Typ hier uw bericht.." rows="5" cols="30">'.$message.'</textarea>';
-//                    }
-//                    else {
-//                        echo '<textarea id="message" name="message" type="text" placeholder="Typ hier uw bericht.." rows="5" cols="30"></textarea>';
-//                    }?>
-<!--                </div>-->
-<!--            </div>-->
+            <div class="row">
+                <div class="twelve wide column">
+                    <label for="input_money">Eigen inbreng (als u geen eigen inbreng heeft vul hier dan 0 in)</label>
+                    <?php if(isset($_GET['input_money'])){
+                        $input_money = $_GET['input_money'];
+                        echo '<input id="input_money" name="input_money" type="text" placeholder="€" value="'.$input_money.'">';
+                    }
+                    else {
+                        echo '<input id="input_money" name="input_money" type="text" placeholder="€">';
+                    }?>
+                </div>
+            </div>
 
             <div class="row">
                 <div class="twelve wide column">
-                    <button id="submit" type="submit" name="submit" value="submit" class="twelve wide blue button">Versturen</button>
+                    <label for="dept">Schulden (als u geen schulden heeft vul hier dan 0 in)</label>
+                    <?php if(isset($_GET['dept'])){
+                        $dept = $_GET['dept'];
+                        echo '<input id="dept" name="dept" type="text" placeholder="€" value="'.$dept.'">';
+                    }
+                    else {
+                        echo '<input id="dept" name="dept" type="text" placeholder="€">';
+                    }?>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="twelve wide column">
+                    <label for="purchase_price">Koopprijs</label>
+                    <?php if(isset($_GET['purchase_price'])){
+                        $purchase_price = $_GET['purchase_price'];
+                        echo '<input id="purchase_price" name="purchase_price" type="text" placeholder="€" value="'.$purchase_price.'">';
+                    }
+                    else {
+                        echo '<input id="purchase_price" name="purchase_price" type="text" placeholder="€">';
+                    }?>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="twelve wide column">
+                    <label for="email">E-mailadres</label>
+                    <?php if(isset($_GET['email'])){
+                        $email = $_GET['email'];
+                        echo '<input id="email" name="email" type="text" placeholder="Uw e-mailadres" value="'.$email.'">';
+                    }
+                    else {
+                        echo '<input id="email" name="email" type="text" placeholder="Uw e-mailadres">';
+                    }?>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="twelve wide column">
+                    <label for="mortgage_duration">Hypotheek looptijd</label>
+                    <?php if(isset($_GET['mortgage_duration'])){
+                        $mortgage_duration = $_GET['mortgage_duration'];
+                        echo '<select id="mortgage_duration" name="mortgage_duration" type="text" placeholder="Selecteer..." value="'.$mortgage_duration.'">
+                        <option value="5">5 jaar</option>
+                        <option value="10">10 jaar</option>
+                        <option value="15">15 jaar</option>
+                        <option value="30">30 jaar</option>;
+                        </select>';
+                    }
+                    else {
+                        echo '<select id="mortgage_duration" name="mortgage_duration" type="text" placeholder="Selecteer...">
+                        <option value="5">5 jaar</option>
+                        <option value="10">10 jaar</option>
+                        <option value="15">15 jaar</option>
+                        <option value="30">30 jaar</option>
+                        </select>';
+                    }?>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="twelve wide column">
+                    <button id="submit" type="submit" name="submit" value="submit" class="twelve wide blue button">Bereken</button>
                 </div>
             </div>
         </form>
@@ -72,15 +136,19 @@ require("../jmh/mortgagevalidations.php");
 
             if ($mortgageCheck == 'empty') {
                 echo
-                    $errorboxStart . "You did not fill in all the fields" . $errorboxEnd;
+                    $errorboxStart . "U heeft niet alle velden ingevuld" . $errorboxEnd;
                 exit();
             } elseif ($mortgageCheck == 'invaliddate') {
                 echo
-                    $errorboxStart . "The date format is invalid" . $errorboxEnd;
+                    $errorboxStart . "De datum is niet goed ingevuld. Het formaat is dd/mm/jjjj." . $errorboxEnd;
                 exit();
             } elseif ($mortgageCheck == 'invalidemail') {
                 echo
-                    $errorboxStart . "The e-mail you have used is invalid" . $errorboxEnd;
+                    $errorboxStart . "Het ingevulde e-mailadres is niet geldig" . $errorboxEnd;
+                exit();
+            } elseif ($mortgageCheck == 'number') {
+                echo
+                    $errorboxStart . "Voer een bedrag in afgerond op hele euro's" . $errorboxEnd;
                 exit();
             }
         }
--- jmh/mortgagevalidations.php
+++ jmh/mortgagevalidations.php
@@ -7,7 +7,7 @@ if (isset($_POST['submit'])) {
     $gross_anual_income = $_POST['gross_anual_income'];
     $input_money = $_POST['input_money'];
     $dept = $_POST['dept'];
-    $purchase_price = $_POST['purchace_price'];
+    $purchase_price = $_POST['purchase_price'];
     $email = $_POST['email'];
     $mortgage_duration = $_POST['mortgage_duration'];
 
@@ -15,20 +15,21 @@ if (isset($_POST['submit'])) {
 
     $housing_ratio = 0.265;
     $maximum_gross_mortgage_burden = $gross_anual_income * $housing_ratio;
-    $monthly_payment = $maximum_gross_mortgage_burden / 12;
 
     if ($mortgage_duration == 5) {
-        $key_interest = 0.05;
+        $key_interest = 1.05;
     }
     elseif ($mortgage_duration == 10) {
-        $key_interest = 0.036;
+        $key_interest = 1.036;
     }
     elseif ($mortgage_duration == 15) {
-        $key_interest = 0.04;
+        $key_interest = 1.04;
     }
     else {
-    $key_interest = 0.036;
+    $key_interest = 1.036;
     }
+
+    $mortgage = $maximum_gross_mortgage_burden * $mortgage_duration * $key_interest;
 
     if (empty($birthdate) || empty($email)) {
         header("Location: ../jmh/mortgagerequest.php?mortgagerequest=empty&birthdate=$birthdate&gross_anual_income=$gross_anual_income&input_money=$input_money&dept=$dept&purchase_price=$purchase_price&email=$email&mortgage_duration=$mortgage_duration");
@@ -41,8 +42,12 @@ if (isset($_POST['submit'])) {
         header("Location: ../jmh/mortgagerequest.php?mortgagerequest=invalidemail&birthdate=$birthdate&gross_anual_income=$gross_anual_income&input_money=$input_money&dept=$dept&purchase_price=$purchase_price&email=$email&mortgage_duration=$mortgage_duration");
         exit();
     }
+    elseif (!is_numeric($gross_anual_income) || !is_numeric($dept) || !is_numeric($purchase_price) || !is_numeric($mortgage_duration)) {
+        header("Location: ../jmh/mortgagerequest.php?mortgagerequest=number&birthdate=$birthdate&gross_anual_income=$gross_anual_income&input_money=$input_money&dept=$dept&purchase_price=$purchase_price&email=$email&mortgage_duration=$mortgage_duration");
+        exit();
+    }
     else {
-        header('Location: http://localhost/temp/jmh/index.php');
+        header("Location: ../jmh/mortgagerequested.php?mortgagerequest=number&birthdate=$birthdate&gross_anual_income=$gross_anual_income&input_money=$input_money&dept=$dept&purchase_price=$purchase_price&email=$email&mortgage_duration=$mortgage_duration&mortgage=$mortgage");
         exit();
     }
 
