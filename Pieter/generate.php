626bd73aad168af7ff9acf7116686a7373ee22c2
Pieter Boersma <pieterboersma20@gmail.com>
2020-04-03 20:11:21 +0200

    Done

--- Pieter/generate.php
+++ Pieter/generate.php
@@ -91,6 +91,6 @@
       $pdf->write(5, "Voor meer informatie over onze gegevensverwerking verwijzen wij u graag door naar onze\n");
 
       $pdf->SetFont("Arial", "U", 12);
-      $pdf->Cell(0, 4.3, "privacyverklaring.", "", "", "", false, "http://localhost/temp/terms.php");
+      $pdf->Cell(0, 4.3, "privacyverklaring.", "", "", "", false, "http://localhost/temp/privacyverklaring.php");
       $pdf->Output();
 ?>
