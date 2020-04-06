122a28dfbc80e2726baafde15c50f4de431d4079
Pieter Boersma <pieterboersma20@gmail.com>
2020-04-06 17:03:51 +0200

    Gebruikers verwijderen aanpassingen

--- requests/user/delete.php
+++ requests/user/delete.php
@@ -4,10 +4,21 @@
       require("../../cookie.php");
       require("../../database.php");
       require("../../user.php");
-      require("../../validation.php");
+      require("../../validate.php");
 
       if(isset($_POST["email"])){
             $email = $_POST["email"];
             $validation = new validation();
+            if($validation->filter_length($email)){
+                  $database = new Database();
+                  $result = $database->delete("DELETE FROM User WHERE email = ?", array($email));
+                  if($result == true){
+                        print("true");
+                  } else {
+                        print("false");
+                  }
+            } else {
+                  print(json_encode($validation->get_errors()[0]));
+            }
       }
 ?>
