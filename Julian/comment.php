636f6ed34f43cf0e7c74f49100bc0fec03aed0b1
julianhulzebosch1997 <43810186+julianhulzebosch1997@users.noreply.github.com>
2020-04-06 11:05:07 +0200

    sql

--- Julian/comment.php
+++ Julian/comment.php
@@ -1,7 +1,18 @@
 <?php
 
+//voorkomt SQL injection naar database
+function ExtendedAddslash(&$params)
+{
+    foreach ($params as &$var) {
+        is_array($var) ? ExtendedAddslash($var) : $var = addslashes($var);
+    }
 
-require("../includes/navbar.php");
+    //function for every POST variable.
+    ExtendedAddslash($_POST);
+
+
+    require("../includes/navbar.php");
+}
 
 ?>
 
@@ -9,7 +20,7 @@ require("../includes/navbar.php");
     <div class="five wide centered container">
         <div class="ten wide container">
             <h3>Plaats uw opmerking</h3>
-                <textarea id="comment" placeholder="Typ hier uw bericht." rows="5" cols="50"></textarea>
+                <textarea id="message" placeholder="Typ hier uw bericht." rows="5" cols="50"></textarea>
         </div>
     </div>
 </div>
