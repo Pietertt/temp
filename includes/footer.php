d4366954caf48b211b82d9fa7d4e39b43992fe95
Jan Minne <janminne@gmail.com>
2020-04-06 12:28:40 +0200

    Updated footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,24 +2,20 @@
         <div class="eight wide container">
             <div class="row nopad">
                 <div class="three wide column">
-                    <h4>Sitemap </h4>
+                    <h4>Algemeen </h4>
                     <ul>
-                        <li><a href="http://localhost/temp/index.php">Home</a></li>
-                        <li><a href="http://localhost/temp/login.php">Inloggen</a></li>
-                        <li><a href="http://localhost/temp/contact.php">Contact</a></li>
-                        <li><a href="http://localhost/temp/about.php">Over ons</a></li>
-                        <li><a href="http://localhost/temp/overview.php">Overzicht</a></li>
+                        <li><a href="http://localhost/temp/jmh/home.php">Home</a></li>
+                        <li><a href="http://localhost/temp/pieter/index.php">Inloggen</a></li>
+                        <li><a href="http://localhost/temp/diego/registration.php">Registreren</a></li>
+                        <li><a href="http://localhost/temp/julian/overview.php">Overzicht</a></li>
+                        <li><a href="http://localhost/temp/jmh/contact.php">Contact</a></li>
                     </ul>
                 </div>
                 <div class="three wide column">
-                    <h4>Sitemap </h4>
+                    <h4>Hypotheken</h4>
                     <ul>
-                        <li><a href="http://localhost/temp/index.php">Home</a></li>
-                        <li><a href="http://localhost/temp/login.php">Inloggen</a></li>
-                        <li><a href="http://localhost/temp/contact.php">Contact</a></li>
-                        <li><a href="http://localhost/temp/about.php">Over ons</a></li>
-                        <li><a href="http://localhost/temp/overview.php">Overzicht</a></li>
-
+                        <li><a href="http://localhost/temp/jmh/mortgagerequest.php">Hypotheek aanvragen</a></li>
+                        <li><a href="http://localhost/temp/jmh/mortgagecalculator.php">Calculator</a></li>
                     </ul>
                 </div>
             </div>
