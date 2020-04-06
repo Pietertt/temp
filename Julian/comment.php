816f4e90b6a28ce1cb097680e1a2dc2a6e661b6c
Pieter Boersma <pieterboersma20@gmail.com>
2020-04-06 11:18:37 +0200

    Bezig met comments invoegen

--- Julian/comment.php
+++ Julian/comment.php
@@ -1,4 +1,5 @@
 <?php
+require("../includes/navbar.php");
 
 //voorkomt SQL injection naar database
 function ExtendedAddslash(&$params)
@@ -6,32 +7,33 @@ function ExtendedAddslash(&$params)
     foreach ($params as &$var) {
         is_array($var) ? ExtendedAddslash($var) : $var = addslashes($var);
     }
-
     //function for every POST variable.
     ExtendedAddslash($_POST);
-
-
-    require("../includes/navbar.php");
 }
+
+      if(isset($_POST["message"])){
+            $message = $_POST["message"];
+            
+            $database = new Database();
+            $database-
+      }
 
 ?>
 
-<div class="row toppad">
-    <div class="five wide centered container">
-        <div class="ten wide container">
-            <h3>Plaats uw opmerking</h3>
-                <textarea id="message" placeholder="Typ hier uw bericht." rows="5" cols="50"></textarea>
-        </div>
-    </div>
-</div>
-
-<div class="row toppad container">
-<div class="five wide centered column container"></div>
-
-    <div class="two wide container">
-<button class="twelve wide blue button"><a href="../" </a> Verstuur</button>
-    </div>
-</div>
-
+<form method="post" action="">
+      <div class="row toppad">
+            <div class="five wide centered container">
+                  <div class="ten wide container">
+                        <div class="row">
+                              <h3>Plaats uw opmerking</h3>
+                              <textarea id="message" name="message" placeholder="Typ hier uw bericht." rows="5" cols="50"></textarea>
+                        </div>   
+                        <div class="row">
+                              <button class="twelve wide blue button">Verstuur</button>
+                        </div>   
+                  </div>
+            </div>
+      </div>
+</form>
 
 <?php require('../includes/footer.php');?>
